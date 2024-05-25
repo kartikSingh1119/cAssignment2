3dfd475c8732bfc1185f48bad8b27c6b5ade085e
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:38:41 +0530

    Update question4.c

--- question4.c
+++ question4.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main() {
+    // 4. Develop a program that reads a number from the user and prints whether it is positive, negative, or zero.
+    // KartikSingh
+  //2303511530009
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("%d is positive.\n", num);
+    } else if (num < 0) {
+        printf("%d is negative.\n", num);
+    } else {
+        printf("%d is zero.\n", num);
+    }
+
+    return 0;
+}
