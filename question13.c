83bbdd7d48608fcafc2891ad48e91ae3b857cc9e
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:45:51 +0530

    Update question13.c

--- question13.c
+++ question13.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    // 13. Divisibility Checker
+    //kartikSingh
+  //2303511530009
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    if (num2 == 0) {
+        printf("Cannot divide by zero!\n");
+    } else if (num1 % num2 == 0) {
+        printf("%d is divisible by %d\n", num1, num2);
+    } else {
+        printf("%d is not divisible by %d\n", num1, num2);
+    }
+    
+    return 0;
+}
