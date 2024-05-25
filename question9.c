3f9aedaf88f578938d012278153a0f8aab130425
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:42:22 +0530

    Update question9.c

--- question9.c
+++ question9.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main() {
+    // 9. Write a program to calculate the volume of a cuboid.
+    // KartikSingh
+  //2303511530009
+    float length, width, height;
+    printf("Enter length, width, and height of the cuboid: ");
+    scanf("%f %f %f", &length, &width, &height);
+
+    float volume = length * width * height;
+    printf("Volume of the cuboid: %.2f\n", volume);
+
+    return 0;
+}
