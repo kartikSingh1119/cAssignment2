45aefb22cfa8784a5947a24ac7b1e4504aaddde4
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:48:37 +0530

    Update question17.c

--- question17.c
+++ question17.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    // 17. Middle Number Finder
+    //KartikSingh
+  //2303511530009
+    int num1, num2, num3;
+    
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    if ((num1 >= num2 && num1 <= num3) || (num1 <= num2 && num1 >= num3)) {
+        printf("Middle number: %d\n", num1);
+    } else if ((num2 >= num1 && num2 <= num3) || (num2 <= num1 && num2 >= num3)) {
+        printf("Middle number: %d\n", num2);
+    } else {
+        printf("Middle number: %d\n", num3);
+    }
+    
+    return 0;
+}
