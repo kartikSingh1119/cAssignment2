4ea1cae3f7aedb727b82a3122fea11efd4857a39
kartikSingh1119 <kartiksinghx.crew@gmail.com>
2024-05-25 20:43:48 +0530

    Update question11.c

--- question11.c
+++ question11.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    // 11. Age Classifier
+    // kartikSingh
+  //2303511530009
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    if (age < 0) {
+        printf("Invalid age!\n");
+    } else if (age <= 12) {
+        printf("Child\n");
+    } else if (age <= 19) {
+        printf("Teenager\n");
+    } else if (age <= 64) {
+        printf("Adult\n");
+    } else {
+        printf("Senior\n");
+    }
+    
+    return 0;
+}
